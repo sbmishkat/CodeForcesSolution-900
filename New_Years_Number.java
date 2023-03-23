5610401ce4207bc6b32882583f8f8c6b99241146
sbmishkat <118377434+sbmishkat@users.noreply.github.com>
2023-03-23 22:05:14 +0600

    a

--- New_Years_Number.java
+++ New_Years_Number.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class New_Years_Number {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2020 <= arr[i] / 2021) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+
+        }
+        sc.close();
+    }
+
 }
